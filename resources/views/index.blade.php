b01a9978d47199b563ee8e5f4faee1cefd28a20b
Babajide Owosakin <jideowosakin@gmail.com>
2015-06-25 13:15:54 +0200

    Converted the data from stdClass to associative array

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,33 @@
-{{json_encode($data)}}
-{{--@extends('layout')--}}
-{{--@section('title', 'Top Stories')--}}
-{{--@stop--}}
-{{--@section('news_content')--}}
-{{--@section('moreImportant')--}}
-{{--@section('image_url')--}}
-{{--<a href="fullStory"><img width="300" height="169" src="http://www.techsuplex.com/wp-content/uploads/2015/04/Lenovo-Launch-2-300x169.jpg"/></a>--}}
-{{--@endsection--}}
-{{--@section('story_title')--}}
-{{--<p><p><a href="fullStory"><strong>Lenovo launches three smartphones: the P70, S90 and S60 in Nigeria</strong></a></p></p>--}}
-{{--@endsection--}}
-{{--@section('time_release')--}}
-{{--5hrs ago--}}
-{{--@endsection--}}
+@extends('layout')
+@section('title', 'Top Stories')
+@stop
+@section('news_content')
+@section('moreImportant')
+@section('image_url')
+<a href="fullStory"><img width="300" height="169" src="{{$data['timeline_stories']['important'][0]['image_url']}}"/></a>
+@endsection
+@section('story_title')
+<p><p><a href="fullStory"><strong>{{$data['timeline_stories']['important'][0]['title']}}</strong></a></p></p>
+@endsection
+@section('time_release')
+5hrs ago
+@endsection
 
-{{--@section('lessImportant')--}}
-{{--@section('small_image_url')--}}
-{{--<a href="fullStory2"><img width="300" height="194" src="http://www.techsuplex.com/wp-content/uploads/2015/04/g4-fb-300x194.jpg"/></a>--}}
-{{--@endsection--}}
-{{--@section('story_title_here')--}}
-{{--<p><a href="fullStory2"><strong>LG unveils the LG G4</strong></a></p>--}}
-{{--@endsection--}}
-{{--@section('time_release')--}}
-{{--5hrs ago--}}
-{{--@endsection--}}
+@section('lessImportant')
+@section('small_image_url')
+<a href="fullStory2"><img width="300" height="194" src="http://www.techsuplex.com/wp-content/uploads/2015/04/g4-fb-300x194.jpg"/></a>
+@endsection
+@section('story_title_here')
+<p><a href="fullStory2"><strong>LG unveils the LG G4</strong></a></p>
+@endsection
+@section('time_release')
+5hrs ago
+@endsection
 
-{{--@section('noImage')--}}
-{{--@section('story_title')--}}
-{{--<p><a href="fullStory3"><strong>Choosing Between A Chromebook And A Large Android Tablet</strong></a></p>--}}
-{{--@endsection--}}
-{{--@section('time_release')--}}
-{{--7hrs ago--}}
-{{--@endsection--}}
+@section('noImage')
+@section('story_title')
+<p><a href="fullStory3"><strong>Choosing Between A Chromebook And A Large Android Tablet</strong></a></p>
+@endsection
+@section('time_release')
+7hrs ago
+@endsection
