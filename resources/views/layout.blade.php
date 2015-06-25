4d0ebd7dcdc5562d41245eb2e68abff3b0e9d428
Babajide Owosakin <jideowosakin@gmail.com>
2015-06-25 15:49:55 +0200

    Modified the views with live data

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -43,106 +43,53 @@
         </ul>
       </section>
     </nav>
+    </div>
 
 
-     <div class="large-12 small-12 columns">
-     <div class="large-10 small-8 columns"><input type="search" results="7" placeholder="What's happening?" name="searchbox">
-     </div>
-     <div class="large-2 small-4 columns"><a href="#" class="alert button expand">Search</a></div>
-     </div>
+     {{--<div class="large-12 small-12 columns">--}}
+     {{--<div class="large-10 small-8 columns"><input type="search" results="7" placeholder="What's happening?" name="searchbox">--}}
+     {{--</div>--}}
+     {{--<div class="large-2 small-4 columns"><a href="#" class="alert button expand">Search</a></div>--}}
+     {{--</div>--}}
 
-     </div>
-
-     {{--<div class="large-4 small-4 columns"><input type="submit" value="find"></div>--}}
-
+     {{--</div>--}}
 
 
     <div class="row news-item" >
-        @yield('news_content')
+
           <div class="large-12 small-12 columns">
-              <div class="row panel radius">
-              @yield('mostImportant')
-                  <div class="large-12 small-12 columns">@yield('image_url')</div>
-                  <div class="large-12 small-12 columns">@yield('story_title')
-                  </div>
-                  {{--img src="http://placehold.it/300x300&text=[img]"--}}
-                 <span class="label">@yield('time_release')</span>
-              </div>
+             @yield('important_stories')
               <br>
 
-              <div class="row panel radius">
-              @yield('lessImportant')
-                  <div class="large-5 small-6 columns">@yield('small_image_url')</div>
-                  <div class="large-7 small-6 columns">@yield('story_title_here')
-                      {{--<ul class="inline-list">--}}
-                          {{--<li><a href="">Reply</a></li>--}}
-                          {{--<li><a href="">Share</a></li>--}}
-                      {{--</ul>--}}
-
-
-                      {{--<h6>2 Comments</h6>--}}
-                      {{--<div class="row">--}}
-                          {{--<div class="large-2 columns small-3"><img src="http://placehold.it/50x50&text=[img]"/></div>--}}
-                          {{--<div class="large-10 columns"><p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit</p></div>--}}
-                      {{--</div>--}}
-                      {{--<div class="row">--}}
-                          {{--<div class="large-2 columns small-3"><img src="http://placehold.it/50x50&text=[img]"/></div>--}}
-                          {{--<div class="large-10 columns"><p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit</p></div>--}}
-                      {{--</div>--}}
-                  </div>
-                  <span class="label">@yield('time_release')</span>
-              </div>
+              @yield('less_important_stories')
               <br/>
 
 
-              {{--<hr/>--}}
+              {{--Stories with no images--}}
+              @yield('stories_with_no_images')
 
 
-              <div class="row panel radius">
-              @yield('noImage')
-                  {{--<div class="large-2 columns small-3"><img src="http://placehold.it/100x100&text=[img]"/></div>--}}
-                  <div class="large-12 columns small-12">@yield('story_title')
-                      {{--<ul class="inline-list">--}}
-                          {{--<li><a href="">Reply</a></li>--}}
-                          {{--<li><a href="">Share</a></li>--}}
-                      {{--</ul>--}}
-                  </div>
-                  <span class="label">@yield('time_release')</span>
               </div>
+    </div>
 
 
-                  </div>
-              </div>
 
-              <footer class="row">
-                  @yield ('footer_content')
-                    <div class="large-12 columns">
-                        <hr/>
-                        <div class="row">
-                            <div class="large-5 columns">
-                                <p>© NewsPepa.</p>
-                            </div>
-                            {{--<div class="large-7 columns">--}}
-                                {{--<ul class="inline-list right">--}}
-                                    {{--<li><a href="#">Section 1</a></li>--}}
-                                    {{--<li><a href="#">Section 2</a></li>--}}
-                                    {{--<li><a href="#">Section 3</a></li>--}}
-                                    {{--<li><a href="#">Section 4</a></li>--}}
-                                    {{--<li><a href="#">Section 5</a></li>--}}
-                                    {{--<li><a href="#">Section 6</a></li>--}}
-                                {{--</ul>--}}
-                            {{--</div>--}}
-                        </div>
-                    </div>
-                </footer>
+    <footer class="row">
+        <div class="large-12 columns">
+            <hr/>
+            <div class="row">
+                <div class="large-5 columns">
+                    <p>© NewsPepa.</p>
+                </div>
 
-
- <script src="ui_newspaper/js/vendor/jquery.js"></script>
-  <script src="ui_newspaper/js/foundation.min.js"></script>
-  <script>
-    $(document).foundation();
-  </script>
-
+            </div>
+        </div>
+    </footer>
+    <script src="ui_newspaper/js/vendor/jquery.js"></script>
+    <script src="ui_newspaper/js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
 
     </body>
-  </html>
+</html>
