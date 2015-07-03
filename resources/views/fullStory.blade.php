567f0addcee67eba03afe2a6b50c429e1a273da1
Toluwanimi <ifekt4god@gmail.com>
2015-07-03 16:38:32 +0100

    views

--- resources/views/fullStory.blade.php
+++ resources/views/fullStory.blade.php
@@ -22,12 +22,10 @@
       <a href="http://www.facebook.com/plugins/like.php?href={{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}&width&layout=standard&action=like&show_faces=true&share=true&height=80&appId=#################" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=100,width=200');return false;">
       <span class="likespan" id="u_6_i"><span>1 Like</span></span></a></div>
       <div class="shareboxdiv"><div class="sharebox">
-<div class="fb-like" data-href="{{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+      <a class="_15ko touchable _2q8z" href="http://www.facebook.com/plugins/like.php?href={{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}&width&layout=standard&action=like&show_faces=true&share=true&height=80&appId=1681272065426030" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=100,width=200');return false;" id="u_6_c" data-sigil="touchable unlike" data-autoid="autoid_63">Like</a></div>
 
-</div>
-
-      <div class="sharebox"><i class="twittericon"></i><a class="_15kq" href="https://twitter.com/share?url={{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}&text={{$full_story['title']}}&via=newspepa" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">Tweet</a></div>
-      <div class="sharebox"><i class="fbicon"></i><a class="_15kr"  href="https://www.facebook.com/dialog/share?app_id=1681272065426030&display=popup&href={{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}&redirect_uri={{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}" >Share</a></div></div></div></footer>
+      <div class="sharebox"><a class="_15kq" href="https://twitter.com/share?url={{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}&text={{$full_story['title']}}&via=newspepa" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">Tweet</a></div>
+      <div class="sharebox"><a class="_15kr" data-store="{&quot;share_id&quot;:896223087101322,&quot;feedback_source&quot;:1,&quot;behavior&quot;:&quot;custom&quot;}" href="https://www.facebook.com/dialog/share?app_id=1681272065426030&display=popup&href={{url($tc->makeStoryUrl($full_story['title'], $full_story['story_id']))}}" data-sigil="share-popup">Share</a></div></div></div></footer>
 @endforeach
 @stop
 
@@ -37,27 +35,6 @@
 @section('related_content')
 <div class="row panel radius"><b>RECENT STORIES</b></div>
 
-@foreach($data['recent_stories'] as $recent_stories)
-    <div class="row panel radius">
-          <div class="large-5 small-4 columns" style="width: 100%;">
-             <a href="{{url($tc->makeStoryUrl($recent_stories['title'], $recent_stories['story_id']))}}">
-                <div class="smallimage_container">
-                   <div class="smallimage"><img width="120" height="100" src="{{$recent_stories['image_url']}}"/></div>
-                    </a>
-                           {{--<span class="label time-count" style="float:right; align-items: right">{{$tc->getTimeDifference($less_important_story['created_date'])}}|{{$less_important_story['no_of_reads']}}reads</span>--}}
-                  <a href="{{url($tc->makeStoryUrl($recent_stories['title'], $recent_stories['story_id']))}}">
-                   <h1 class="title">{{$recent_stories['title']}} </h1>
-                   </a>
-
-                </div>
-
-</div>
-
-</div>
-
- @endforeach
-
- @stop
 
 <div class="large-12 small-12 medium-12 columns">
 @foreach($data['recent_stories'] as $recent_stories)
