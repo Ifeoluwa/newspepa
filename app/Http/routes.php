ea9bae6653cb1671161c6f1c62ad9d30ff225fb9
Babajide Owosakin <jideowosakin@gmail.com>
2015-07-07 18:17:19 +0200

    Worked on JSON response for the timeline stories

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,8 +13,11 @@
 
 //handles the home page request which displays the top stories/Timeline stories
 
-
 Route::get('/', 'TimelineStoryController@index');
+
+// Handles the response of stories in Json format
+Route::get('/stories_json','TimelineStoryController@getStoriesJson');
+
 
 Route::get('test', 'FeedController@test');
 
