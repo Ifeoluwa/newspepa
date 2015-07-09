6d26197e1c97bb909d487e36b4d8e902374787e6
Toluwanimi <ifekt4god@gmail.com>
2015-07-09 13:10:50 +0100

    layout modified for search results

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -2,18 +2,15 @@
 @section('title', 'results')
 @stop
 
-<span style="font-size:x-large;text-align: center; font-family: Roboto ;float:center"><b>"Search Results"</b></span>
+<span class="title"><b>"Search Results"</b></span>
 
 {{--the search results page also carries the same layout as the index page. Probably will be fetching from the timeline table. so carries
 the important section, lessImportant and noImage stories--}}
 @section('important_stories')
 @endsection
-@section('less_important_stories')
-@endsection
-@section('stories_with_no_images')
-@endsection
+
 @section('related_content')
-  <div class="row panel radius"><b>Trending Searches</b></div>
-  <div class="row panel radius"><b>Related Searches</b></div>
-  <div class="row panel radius"><b>Latest in Category</b></div>
+  <div class="row panel radius related-content"><b>Trending Searches</b></div>
+  <div class="row panel radius related-content"><b>Related Searches</b></div>
+  {{--<div class="row panel radius"><b>Latest in Category</b></div>--}}
 @endsection
