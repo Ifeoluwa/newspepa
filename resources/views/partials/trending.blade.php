74473ca9c0915ade9c19a7d45ef68669428465f8
Toluwanimi <ifekt4god@gmail.com>
2015-08-24 17:33:44 +0100

    trending box changed, comment box adjusted

--- resources/views/partials/trending.blade.php
+++ resources/views/partials/trending.blade.php
@@ -5,7 +5,7 @@
         <?php $counter = 1; ?>
         @foreach($data['category_stories']['trending'] as $trending)
         <div  id="{{'trend'.$counter}}" class="large-12 small-12 medium-12 columns column-trending">
-            <a href="{{url($tc->makeStoryUrl($trending['title'], $trending['story_id']))}}"><span class="title-timeline">{{$trending['title']}}</span></a>
+            <a href="{{url($tc->makeStoryUrl($trending['title'], $trending['story_id']))}}"><span class="title-timeline" style="font-weight: normal">{{$trending['title']}}</span></a>
         </div>
         <?php $counter += 1; ?>
         @endforeach
